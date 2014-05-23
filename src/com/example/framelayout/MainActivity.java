399332060032fcc5be2b77d0d78264fa535dfb31
Bootez <8ootez@gmail.com>
2014-05-23 14:26:18 +0800

    Test FrameLayout

--- src/com/example/framelayout/MainActivity.java
+++ src/com/example/framelayout/MainActivity.java
@@ -1,15 +1,74 @@
 package com.example.framelayout;
 
-import android.os.Bundle;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    
+    private final int[] colors = new int[]  {
+            R.color.red,
+            R.color.orange,
+            R.color.yellow,
+            R.color.green,
+            R.color.blue,
+            R.color.indigo,
+            R.color.purple,
+    };
+    
+    private final int[] ids = new int[] {
+            R.id.view01,
+            R.id.view02,
+            R.id.view03,
+            R.id.view04,
+            R.id.view05,
+            R.id.view06,
+            R.id.view07,
+    };
+    
+    private int currentColor = 0;
+    
+    TextView[] views = new TextView[ids.length];
+    
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) 
+        {
+            if (msg.what == 0x123) {
+                for (int i = 0; i < ids.length; i++) {
+                    views[i].setBackgroundResource(colors[(currentColor + i) % colors.length]);
+                }
+                
+                currentColor++;
+            } 
+            
+            super.handleMessage(msg);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        for (int i = 0; i < ids.length; i++) {
+            views[i] = (TextView) findViewById(ids[i]);
+        }
+        
+        new Timer().schedule(new TimerTask() {
+            
+            @Override
+            public void run() {
+                handler.sendEmptyMessage(0x123);
+            }
+        }, 0, 200);
     }
 
     @Override
